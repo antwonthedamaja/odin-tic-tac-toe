2197900ce28e773328504ffa092d930455b91b44
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-16 20:04:21 -0400

    Move function to be a part of main object

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ const game = {
     gameBoard: ['x', 'x', 'x',
                 'x', 'x', 'x',
                 'x', 'o', 'o'],
-}
-
-function renderBoard() {
-    tiles.forEach((tile, index) => {
-        tile.innerText = game.gameBoard[index];
-      });
+    renderBoard: function() {
+        tiles.forEach((tile, index) => {
+            tile.innerText = this.gameBoard[index];
+          });
+    }
 }
